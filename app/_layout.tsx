1d493d0d6bb062dad2b7fa872988d0cc6199f180
Komal Kaur <kkaur5@mail.sfsu.edu>
2024-08-30 14:04:46 -0700

    File Clean Up

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
 import { Text, View } from "react-native";
+import WeatherApp from '../components/WeatherApp';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function Home() {
+  return <WeatherApp />;
 }
